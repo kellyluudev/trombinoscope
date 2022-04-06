d414f471f15d379ef8f95d30abbd7dd7786a373b
Kelly Luu <kelly.luu@groupeseloger.com>
2022-04-06 11:41:22 +0200

    Add images

--- app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
@@ -3,12 +3,7 @@ package aviv.workshop.trombinoscope
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import aviv.workshop.trombinoscope.ui.theme.TrombinoscopeTheme
 
@@ -17,27 +12,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TrombinoscopeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TrombinoscopeTheme {
-        Greeting("Android")
+
     }
 }
