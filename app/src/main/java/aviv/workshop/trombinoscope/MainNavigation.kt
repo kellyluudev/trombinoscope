6952afe731c60a43294a7c60d4a9b3e2b3334124
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 18:22:09 +0200

    Add navHost with dependency
    Note-  Its a breaking commit, but its important to show addition of dependency

--- app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
@@ -1,7 +1,11 @@
 package aviv.workshop.trombinoscope
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
 
 @Composable
 fun MainNavigation() {
+    NavHost() {
+
+    }
 }
