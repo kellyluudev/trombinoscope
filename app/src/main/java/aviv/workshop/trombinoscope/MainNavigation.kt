2e2c44708d7493154f02540f8f9d21a032af7a83
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 18:43:51 +0200

    Add WorkerScreen
    Note- Breaking change, we will adjust code in the upcoming commits

--- app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
@@ -1,10 +1,12 @@
 package aviv.workshop.trombinoscope
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import aviv.workshop.trombinoscope.details.DetailsScreen
+import aviv.workshop.trombinoscope.list.WorkerScreen
 
 @Composable
 fun MainNavigation() {
@@ -13,14 +15,14 @@ fun MainNavigation() {
         navController = hostController,
         startDestination = Screen.WorkerListScreen.route
     ) {
-//        composable(route = Screen.WorkerListScreen.route) {
-//            WorkerScreen(
-//                viewModel = viewModel(),
-//                navigateToDetails = {
-//                    hostController.navigate(Screen.DetailScreen.route)
-//                }
-//            )
-//        }
+        composable(route = Screen.WorkerListScreen.route) {
+            WorkerScreen(
+                viewModel = viewModel(),
+                navigateToDetails = {
+                    hostController.navigate(Screen.DetailScreen.route)
+                }
+            )
+        }
 
         composable(route = Screen.DetailScreen.route) {
             DetailsScreen(name = "name", jobTitle = "job title")
