0cec018ed1800542fdfb3dd39aa92ce484fd9cf3
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 11:26:18 +0200

    Add setContent with theme

--- app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
@@ -3,10 +3,23 @@ package aviv.workshop.trombinoscope
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import com.example.figma.theme.TrombinoscopeTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            TrombinoscopeTheme {
+                WorkerScreen()
+            }
+        }
+    }
+
+    @Composable
+    private fun WorkerScreen() {
+        TODO("Not yet implemented")
     }
 }
