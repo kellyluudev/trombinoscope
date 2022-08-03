c55c4772dd3c7511021f9e2afebc9fe92e16dec4
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 14:35:20 +0200

    Set round picture using clip property

--- app/src/main/java/aviv/workshop/trombinoscope/MainComposables.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainComposables.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,7 +47,11 @@ private fun WorkerItem(worker: Worker) =
             }
             Image(
                 painter = painterResource(worker.pictureRes),
-                contentDescription = "Worker picture"
+                contentDescription = "Worker picture",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(8.dp))
             )
         }
     }
