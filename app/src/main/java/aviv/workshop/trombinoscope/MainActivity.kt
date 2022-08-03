970c5e1a214751de44f16008cc1b95963ff083ca
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 18:47:44 +0200

    Adjust MainActivity to use MainNavigation; add a preview
    Note- Breaking change, we will adjust code in the upcoming commits

--- app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
@@ -3,23 +3,24 @@ package aviv.workshop.trombinoscope
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
-
-    private val viewModel = MainViewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TrombinoscopeTheme {
-                WorkerScreen(viewModel.getWorkers())
+                MainNavigation()
             }
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    TrombinoscopeTheme {
+        MainNavigation()
     }
 }
