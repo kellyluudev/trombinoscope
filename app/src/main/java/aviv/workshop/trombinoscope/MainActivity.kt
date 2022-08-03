9cf2ad80e9c39b033230fb086a6c011f101aa249
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 11:23:09 +0200

    Add empty onCreate
    Why: There are two onCreate; and the wrong one os proposed by the IDE as first option which in not called by the Android lifecycle. So for ease of workshop, add correct onCreate

--- app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
@@ -1,5 +1,12 @@
 package aviv.workshop.trombinoscope
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity()
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
