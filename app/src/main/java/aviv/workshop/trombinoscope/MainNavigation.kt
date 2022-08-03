48851ea3a4afa89e6ffe060f81d6d001badd0662
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 18:42:50 +0200

    Add details screen

--- app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
@@ -1,15 +1,34 @@
 package aviv.workshop.trombinoscope
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import aviv.workshop.trombinoscope.details.DetailsScreen
 
 @Composable
 fun MainNavigation() {
-    val navController = rememberNavController()
+    val hostController = rememberNavController()
     NavHost(
-
+        navController = hostController,
+        startDestination = Screen.WorkerListScreen.route
     ) {
+//        composable(route = Screen.WorkerListScreen.route) {
+//            WorkerScreen(
+//                viewModel = viewModel(),
+//                navigateToDetails = {
+//                    hostController.navigate(Screen.DetailScreen.route)
+//                }
+//            )
+//        }
 
+        composable(route = Screen.DetailScreen.route) {
+            DetailsScreen(name = "name", jobTitle = "job title")
+        }
     }
+}
+
+sealed class Screen(val route: String) {
+    object WorkerListScreen : Screen("worker_list_screen")
+    object DetailScreen : Screen("detail_screen")
 }
