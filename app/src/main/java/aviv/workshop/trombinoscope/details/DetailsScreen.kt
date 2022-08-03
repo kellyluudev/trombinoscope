5643fa86dfe56dcde5ee37bb6222dfff64996f62
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 18:15:59 +0200

    Add components to details screen
    Note- We overlook details design for now; as goal is navigation

--- app/src/main/java/aviv/workshop/trombinoscope/details/DetailsScreen.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/details/DetailsScreen.kt
@@ -1,8 +1,13 @@
 package aviv.workshop.trombinoscope.details
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import com.example.figma.atoms.H3Text
 
 @Composable
-fun DetailsScreen() {
-
+fun DetailsScreen(name: String, jobTitle: String) {
+    Column {
+        H3Text("Name: $name")
+        H3Text("Job title: $jobTitle")
+    }
 }
