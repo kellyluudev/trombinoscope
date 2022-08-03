ca4006c39dd1881826c603fd820b766e0a3256e2
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 11:15:17 +0200

    Add ButtonComponent in design

--- app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
@@ -1,23 +1,5 @@
 package aviv.workshop.trombinoscope
 
-import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import aviv.workshop.trombinoscope.ui.theme.TrombinoscopeTheme
 
-class MainActivity : ComponentActivity() {
-
-}
+class MainActivity : ComponentActivity()
