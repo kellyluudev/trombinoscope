ecae829c2bc669368629296bea0fc8726d508bbc
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 14:14:55 +0200

    Add Image

--- app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainActivity.kt
@@ -3,10 +3,15 @@ package aviv.workshop.trombinoscope
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 
 class MainActivity : ComponentActivity() {
 
@@ -37,6 +42,10 @@ class MainActivity : ComponentActivity() {
             ) {
                 Text(text = "Show details")
             }
+            Image(
+                painter = painterResource(worker.pictureRes),
+                contentDescription = "Worker picture"
+            )
         }
     }
 }
