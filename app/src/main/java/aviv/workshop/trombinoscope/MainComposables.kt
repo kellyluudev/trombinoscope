a6e81e705ccd05f28859102c34e440a17c93ef49
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-03 16:10:24 +0200

    Add padding between each item

--- app/src/main/java/aviv/workshop/trombinoscope/MainComposables.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainComposables.kt
@@ -29,11 +29,12 @@ fun WorkerScreen(workers: List<Worker>) {
 }
 
 @Composable
-private fun WorkerList(workers: List<Worker>) {
-    LazyColumn(modifier = Modifier.padding(16.dp)) {
-        items(workers) {
-            WorkerItem(worker = it)
-        }
+private fun WorkerList(workers: List<Worker>) = LazyColumn(
+    modifier = Modifier.padding(16.dp),
+    verticalArrangement = Arrangement.spacedBy(8.dp)
+) {
+    items(workers) {
+        WorkerItem(worker = it)
     }
 }
 
