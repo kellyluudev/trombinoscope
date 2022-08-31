071165acf1dd45f5441b542fd91969b24f70eac4
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-31 11:07:00 +0200

    Extract methods to name composables added to the navigation
    Why: for readability

--- app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/MainNavigation.kt
@@ -2,12 +2,12 @@ package aviv.workshop.trombinoscope
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import aviv.workshop.trombinoscope.details.DetailsScreen
+import aviv.workshop.trombinoscope.list.Worker
 import aviv.workshop.trombinoscope.list.WorkerListScreen
 
 @Composable
@@ -17,26 +17,33 @@ fun MainNavigation() {
         navController = hostController,
         startDestination = Screen.WORKER_LIST.name
     ) {
-        composable(route = Screen.WORKER_LIST.name) {
-            WorkerListScreen(
-                viewModel = viewModel(),
-                onItemClicked = { worker -> hostController.navigate(Screen.WORKER_DETAIL.name) }
-            )
-        }
-
-        composable(route = Screen.WORKER_DETAIL.name, arguments = listOf(
-            navArgument("worker") {
-                type = NavType.ReferenceType
-                defaultValue = "Unknown"
-                nullable = false
-            }
-        )) {
-            DetailsScreen(name = "name", jobTitle = "job title")
-        }
+        addWorkerList(hostController)
+        addWorkerDetails()
     }
 }
+
+private fun NavGraphBuilder.addWorkerList(hostController: NavHostController) =
+    composable(route = Screen.WORKER_LIST.name) {
+        WorkerListScreen(
+            viewModel = viewModel(),
+            onItemClicked = { worker -> hostController.navigate(Screen.WORKER_DETAIL.name) }
+        )
+    }
+
+private fun NavGraphBuilder.addWorkerDetails() =
+    composable(route = Screen.WORKER_DETAIL.name, arguments = listOf(
+        navArgument("worker") {
+            type = NavType.ReferenceType
+            defaultValue = "Unknown"
+            nullable = false
+        }
+    )) {
+        DetailsScreen(name = "name", jobTitle = "job title")
+    }
 
 private enum class Screen {
     WORKER_LIST,
     WORKER_DETAIL
 }
+
+private class WorkerExtras(worker: Worker) : Navigator.Extras
