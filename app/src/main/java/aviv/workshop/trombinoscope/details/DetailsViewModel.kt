ab1103d28eac0daf64615f36cff695953e21bd85
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-31 15:11:28 +0200

    Add return type

--- app/src/main/java/aviv/workshop/trombinoscope/details/DetailsViewModel.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/details/DetailsViewModel.kt
@@ -1,9 +1,10 @@
 package aviv.workshop.trombinoscope.details
 
 import androidx.lifecycle.ViewModel
+import aviv.workshop.trombinoscope.list.Worker
 
 class DetailsViewModel : ViewModel() {
-    fun getWorker(id: Int) {
+    fun getWorker(id: Int): Worker {
         TODO("Not yet implemented")
     }
 }
