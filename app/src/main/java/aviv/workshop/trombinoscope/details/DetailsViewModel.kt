ec498ab967e7ba76a2a8e03e16086fced9a598f2
iwb-bb <bhumanyu.barkakati@immoweb.be>
2022-08-31 15:15:37 +0200

    Add worker repository

--- app/src/main/java/aviv/workshop/trombinoscope/details/DetailsViewModel.kt
+++ app/src/main/java/aviv/workshop/trombinoscope/details/DetailsViewModel.kt
@@ -2,8 +2,12 @@ package aviv.workshop.trombinoscope.details
 
 import androidx.lifecycle.ViewModel
 import aviv.workshop.trombinoscope.list.Worker
+import aviv.workshop.trombinoscope.list.WorkerRepository
 
 class DetailsViewModel : ViewModel() {
+
+    private val repository = WorkerRepository()
+
     fun getWorker(id: Int): Worker {
         TODO("Not yet implemented")
     }
